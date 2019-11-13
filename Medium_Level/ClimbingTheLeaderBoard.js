626c1f1b4d50b4e22e766c16d585845f9a06ba5d
Kenneth <sally@sallyde-MacBook-Air.local>
2019-11-13 13:33:11 -0800

    initial commit

---
+++ Medium_Level/ClimbingTheLeaderBoard.js
@@ -0,0 +1,23 @@
+// Question
+
+// sample input array one: leaderboard: 100, 100, 50, 40, 40, 20, 10
+// array of alice: 5, 25, 50, 120 (in ascending order)
+
+// ouput has to be an array of alice's ranking each game
+// 6,4,2,1
+
+const climbingLeaderboard = function(scores, alice) {
+  let result = [];
+  const newScores = [...new Set(scores)];
+  let size = newScores.length - 1;
+
+  for (let i = 0; i < alice.length; i++) {
+    while (newScores[size] <= alice[i]) {
+      size--;
+    }
+    result.push(size + 2);
+  }
+  return result;
+};
+
+console.log(climbingLeaderboard([100,100,50,40,40,20,10],[5,25,50,120]))
