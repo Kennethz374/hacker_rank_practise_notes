739609eca73f739ca6a59f9844332584ecab8bb2
Kenneth <sally@sallyde-MacBook-Air.local>
2019-12-15 19:52:34 -0800

    added compareTriplets

---
+++ Easy_Level/CompareTriplets.js
@@ -0,0 +1,16 @@
+function compareTriplets(a, b) {
+  const result = [];
+  let alice = 0;
+  let bob = 0;
+  for (let i = 0; i < a.length; i++) {
+      if (a[i] > b[i]) {
+          alice += 1
+      } 
+      if (a[i] < b[i]) {
+          bob += 1
+      }
+  }
+  result.push(alice)
+  result.push(bob)
+  return result
+}
