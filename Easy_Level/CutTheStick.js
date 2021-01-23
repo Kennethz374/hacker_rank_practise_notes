8bec83a0588d5b90b551a1628ecf812058a8817f
Kenneth <KennethZhang374@gmail.com>
2021-01-22 23:02:50 -0800

    commit

--- Easy_Level/CutTheStick.js
+++ Easy_Level/CutTheStick.js
@@ -1,1 +1,21 @@
 // Cut The Sticks//
+//ex  ar = [1,2,3]
+//find the shortest stick, remove it, and minus all other sticks with the shortest one's length
+//first iteration ar = [1,2]   ===>  ar = [1]
+//count the number of sticks left in each iteration
+//which is ans = [3,2,1]
+
+let ar = [5, 4, 4, 2, 2, 8];
+let ans = ar.filter(a => a !== 4);
+
+const CutTheStick = arr => {
+  let ans = [arr.length];
+  for (let i = arr.length; i >= 0; i--) {
+    let min = Math.min(...arr);
+    arr = arr.filter(x => x !== min);
+    ans.push(arr.length);
+  }
+  return ans.filter(x => x !== 0);
+};
+
+console.log(CutTheStick(ar));
