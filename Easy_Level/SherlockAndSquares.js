1cb8e2a88be57416fa9c5aba4b83b0f64c714842
Kenneth <KennethZhang374@gmail.com>
2021-01-23 22:59:43 -0800

    change logic

--- Easy_Level/SherlockAndSquares.js
+++ Easy_Level/SherlockAndSquares.js
@@ -6,17 +6,42 @@
 
 // There are three square integers in the range:  and . Return .
 
-const s = 3;
-const e = 9;
+const s = 10;
+const e = 1000;
+
+const a = 3;
+const b = 9;
 
 const squares = (a, b) => {
-  let ans = 0;
-  for (let i = a; i <= b; i++) {
-    if (Number.isInteger(Math.sqrt(i))) {
-      ans += 1;
-    }
+  // let ans = 0;
+  // for (let i = a; i <= b; i++) {
+  //   if (Number.isInteger(Math.sqrt(i))) {
+  //     ans += 1;
+  //   }
+  // }
+  // return ans;
+
+  let highestInt = Math.floor(Math.sqrt(b));
+  let lowestInt = Math.ceil(Math.sqrt(a));
+  console.log(highestInt, lowestInt);
+  let ans = highestInt - lowestInt;
+  if (Number.isInteger(Math.sqrt(b))) {
+    ans += 1;
   }
+  if (Number.isInteger(Math.sqrt(a))) {
+    ans += 1;
+  }
+  if (ans <= 0) {
+    return 0;
+  }
+
   return ans;
+  // if (a) {
+  //   return ans;
+  // } else {
+  //   return 0;
+  // }
 };
 
 console.log(squares(s, e));
+console.log(squares(a, b));
