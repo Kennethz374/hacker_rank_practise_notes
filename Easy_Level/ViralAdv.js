2147d0950e1be446f9b55ca0161feccd8269e70e
Kenneth <KennethZhang374@gmail.com>
2021-01-25 11:43:44 -0800

    finish

---
+++ Easy_Level/ViralAdv.js
@@ -0,0 +1,16 @@
+let day = 4;
+
+const viralAd = n => {
+  let sum = 0;
+  let todayRecipents = 5;
+  let todayLikes = 0;
+  for (let i = 1; i <= n; i++) {
+    todayLikes = Math.floor(todayRecipents / 2);
+    sum = sum + todayLikes;
+    todayRecipents = todayLikes * 3;
+  }
+  console.log(sum, todayLikes);
+  return sum;
+};
+
+console.log(viralAd(day));
